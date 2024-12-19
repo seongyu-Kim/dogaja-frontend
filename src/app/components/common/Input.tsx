593a56977da15dfafc9af309e7b384acd7c1f4b4
[React1] 김선규 <seongyu1066@naver.com>
2024-12-19 17:08:50 +0900

    feat: 회원가입 페이지 구현

--- src/app/components/common/Input.tsx
+++ src/app/components/common/Input.tsx
@@ -4,7 +4,7 @@ interface InputProps {
   type: string;
   name: string;
   placeholder?: string;
-  value: string;
+  value?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   required?: boolean;
   className?: string;
@@ -27,7 +27,7 @@ const Input: React.FC<InputProps> = ({
       value={value}
       onChange={onChange}
       required={required}
-      className={`p-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-green-300 ${className}`}
+      className={`p-1 border border-gray-300 rounded-lg focus:outline-none  ${className}`}
     />
   );
 };
