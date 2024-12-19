156ab27dc455c3927afb351c13636a42543e1323
[React1] 김선규 <seongyu1066@naver.com>
2024-12-19 13:49:38 +0900

    feat: form, input 공통 컴포넌트 생성

---
+++ src/app/components/common/Input.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface InputProps {
+  type: string;
+  name: string;
+  placeholder?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  required?: boolean;
+  className?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  required = false,
+  className = "",
+}) => {
+  return (
+    <div className="flex flex-col">
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        required={required}
+        className={`p-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-green-300 ${className}`}
+      />
+    </div>
+  );
+};
+
+export default Input;
