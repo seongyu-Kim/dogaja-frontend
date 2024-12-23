b7cc3f934eedd6f2e092f58bccecac7dc77fc68d
[React1] 박주호 <parkjh001217@gmail.com>
2024-12-23 18:02:29 +0900

    Feat: 게시판 확인 할 수 있는 페이지 제작 및 리스트 요소 수정

--- src/app/(route)/board/page.tsx
+++ src/app/(route)/board/page.tsx
@@ -1,20 +1,178 @@
-// app/board/page.tsx
+"use client";
+
 import Link from "next/link";
 import { BOARD_TYPES } from "@/app/utils/board-config";
+import List from "@/app/components/common/List";
+
+const testList = [
+  // 임시 테스트 리스트
+  {
+    id: 1,
+    title: "1번",
+    name: "여기저기",
+    image_id: null,
+    //임시
+    commentCount: 6,
+  },
+  {
+    id: 2,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 3,
+    title: "1번",
+    name: "여기저기",
+    image_id: null,
+    //임시
+    commentCount: 6,
+  },
+  {
+    id: 4,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 5,
+    title: "1번",
+    name: "여기저기",
+    image_id: null,
+    //임시
+    commentCount: 6,
+  },
+  {
+    id: 6,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 7,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 8,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 9,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 10,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 11,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 12,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 13,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 14,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 15,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 16,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 17,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+  {
+    id: 18,
+    title: "3번",
+    name: "이거저거",
+    image_id: "aa",
+    //임시
+    commentCount: 125,
+  },
+];
 
 const BoardListPage = () => {
   return (
-    <div>
-      <h1>게시판 목록</h1>
-      <ul>
+    <div className="flex flex-col items-center">
+      <div className="grid grid-cols-2 gap-4 w-1/2">
         {Object.keys(BOARD_TYPES).map((boardType) => (
-          <li key={boardType}>
+          <div key={boardType} className="p-4 border rounded-md">
             <Link href={`/board/${boardType}`}>
-              {BOARD_TYPES[boardType as keyof typeof BOARD_TYPES].title} 게시판
+              <p className="border-b border-gray-400 text-3xl">
+                {BOARD_TYPES[boardType as keyof typeof BOARD_TYPES].title}{" "}
+                게시판
+              </p>
+              <div>
+                <List
+                  list={testList.sort((a, b) => a.id - b.id).slice(0, 10)}
+                  preview={false}
+                />
+              </div>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
