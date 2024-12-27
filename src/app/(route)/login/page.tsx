fa7484ab7281478bb98215f381a554f59ec6c51a
[React1] 김선규 <seongyu1066@naver.com>
2024-12-27 22:39:38 +0900

    refactor: react-hook-form을 사용한 폼 처리

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 import Input from "@/app/components/common/Input";
 import Button from "@/app/components/common/Button";
 import Logo from "@/app/assets/Do_logo_non_text.png";
@@ -9,71 +10,54 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { mainApi } from "@/app/utils/mainApi";
 import { API } from "@/app/utils/api";
-import { SuccessAlert } from "@/app/utils/toastAlert";
-import { useUserStore } from "@/app/store/userStore";
+import { SuccessAlert, ErrorAlert } from "@/app/utils/toastAlert";
 
-const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const { fetchUser } = useUserStore();
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({ mode: "onChange" });
   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const emailPattern =
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-
-    if (!email) {
-      setError("이메일을 입력해주세요.");
-      return;
-    }
-
-    if (!emailPattern.test(email)) {
-      setError("올바르지 않은 이메일 형식입니다");
-      return;
-    }
-    if (!password) {
-      setError("비밀번호를 입력해주세요.");
-      return;
-    }
-
-    setError(null);
-    setIsLoading(true);
-
+  const onSubmit = async (formData: LoginFormData) => {
     try {
       const res = await mainApi<{ accessToken: string }>({
         url: API.AUTH.LOGIN,
         method: "POST",
-        data: { email, password, role: "user" },
+        data: {
+          email: formData.email,
+          password: formData.password,
+          role: "user",
+        },
       });
 
       if (res.status === 201) {
         SuccessAlert("로그인 성공");
         localStorage.setItem("token", res.data.accessToken);
-        await fetchUser();
         router.push("/dashboard");
       }
     } catch (e) {
       console.error("로그인 실패", e);
       if (e.status === 401) {
-        setError("등록되지 않은 사용자입니다.");
-      } else if (e.status === 404) {
-        setError("비밀번호가 일치하지 않습니다.");
+        ErrorAlert("이메일 또는 비밀번호가 일치하지 않습니다.");
+      } else if (e.status === 403) {
+        ErrorAlert("관리자 권한이 존재하지 않습니다.");
       } else {
-        setError("로그인 중 오류가 발생했습니다.");
+        ErrorAlert("로그인 중 오류가 발생했습니다.");
       }
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
     <div className="flex items-center min-h-screen">
-      <div className="max-w-md w-full mx-auto p-4 border rounded-md shadow-xl drop-shadow-sm h-[600px] relative">
-        <form onSubmit={handleSubmit}>
+      <div className="max-w-md w-full mx-auto p-4 border rounded-md shadow-xl drop-shadow-sm h-[600px]">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Image
             src={Logo}
             alt="로고이미지"
@@ -87,42 +71,55 @@ const Login: React.FC = () => {
           </p>
 
           <div className="space-y-4 mt-4 w-10/12 mx-auto">
+            {/* 이메일 */}
             <div className="flex flex-col">
               <label className="ml-2">이메일</label>
               <Input
-                type="string"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="example@email.com"
+                {...register("email", {
+                  required: "이메일을 입력해주세요",
+                  pattern: {
+                    value:
+                      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+                    message: "올바른 이메일 형식이 아닙니다",
+                  },
+                })}
                 className="focus:ring-1 focus:ring-green-300"
               />
+              {errors.email && (
+                <p className="ml-1 text-red-500 text-sm">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
-            <div className="flex flex-col">
+            {/* 비밀번호 */}
+            <div className="flex flex-col relative">
               <label className="ml-2">비밀번호</label>
               <Input
                 type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호를 입력하세요"
+                {...register("password", {
+                  required: "비밀번호를 입력해주세요",
+                })}
                 className="focus:ring-1 focus:ring-green-300"
               />
-            </div>
-
-            <div className="flex justify-end">
-              <Link
-                href="/find-password"
-                className="text-xs text-gray-500 mr-1 -mt-2"
-              >
-                비밀번호 찾기
-              </Link>
+              {errors.password && (
+                <p className="ml-1 text-red-500 text-sm">
+                  {errors.password.message}
+                </p>
+              )}
+              <div className="absolute right-0 top-14">
+                <Link href="/find-password" className="text-xs text-gray-500">
+                  비밀번호 찾기
+                </Link>
+              </div>
             </div>
           </div>
-          {error && (
-            <div className="ml-9 -mt-4 text-sm text-red-500">{error}</div>
-          )}
 
-          <div className="flex flex-col items-center mt-16">
+          {/* 로그인 버튼 */}
+          <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
             <Button
               style={{
                 hoverColor: "hover:bg-[#3CB731]",
@@ -130,13 +127,14 @@ const Login: React.FC = () => {
                 width: "w-52",
               }}
               type="submit"
-              disabled={isLoading}
+              disabled={isSubmitting}
             >
               로그인
             </Button>
           </div>
 
-          <div className="text-sm mt-1 text-gray-500 text-center">
+          {/* 회원가입 */}
+          <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 text-sm mt-1 text-gray-500 text-center">
             <Link href="/sign-up">회원가입</Link>
           </div>
         </form>
--- src/app/components/common/Input.tsx
+++ src/app/components/common/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 interface InputProps {
   type: string;
@@ -8,34 +8,44 @@ interface InputProps {
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   onClick?: React.MouseEventHandler<HTMLInputElement>;
   onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
   required?: boolean;
   className?: string;
 }
 
-const Input: React.FC<InputProps> = ({
-  type,
-  name,
-  placeholder,
-  value,
-  onChange,
-  onClick,
-  onKeyDown,
-  required = false,
-  className = "",
-}) => {
-  return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onClick={onClick}
-      onKeyDown={onKeyDown}
-      required={required}
-      className={`p-1 border border-gray-300 rounded-lg focus:outline-none ${className}`}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      type,
+      name,
+      placeholder,
+      value,
+      onChange,
+      onClick,
+      onKeyDown,
+      onBlur,
+      required = false,
+      className = "",
+    },
+    ref
+  ) => {
+    return (
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onClick={onClick}
+        onKeyDown={onKeyDown}
+        onBlur={onBlur}
+        ref={ref}
+        required={required}
+        className={`p-1 border border-gray-300 rounded-lg focus:outline-none ${className}`}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
 
 export default Input;
