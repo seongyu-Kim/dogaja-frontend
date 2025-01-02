3c64cbac6882736148469425758cc993315384ff
[React1] 김선규 <seongyu1066@naver.com>
2025-01-02 22:27:32 +0900

    refactor: 검색어 매칭 컴포넌트 분리

---
+++ src/app/utils/highlightMatch.tsx
@@ -0,0 +1,16 @@
+export const highlightMatch = (text: string, search: string) => {
+  if (!search) return text;
+
+  const regex = new RegExp(`(${search})`, "g");
+  const parts = text.split(regex);
+
+  return parts.map((part, index) =>
+    part === search ? (
+      <span key={index} className="text-green-500">
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
+};
