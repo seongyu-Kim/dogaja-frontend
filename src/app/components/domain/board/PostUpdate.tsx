b92f7d3a09555225cf7ff2d9aa37867724e62ebe
[React1] 박주호 <parkjh001217@gmail.com>
2025-01-03 23:23:10 +0900

    Fix: 오피스아워 피드백 반영

--- src/app/components/domain/board/PostUpdate.tsx
+++ src/app/components/domain/board/PostUpdate.tsx
@@ -2,71 +2,53 @@
 
 import Input from "@/app/components/common/Input";
 import React, { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { modules } from "@/app/utils/reactQuillOptions";
 import Button from "@/app/components/common/Button";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { ErrorAlert, SuccessAlert } from "@/app/utils/toastAlert";
 import { updatePost } from "@/app/utils/boardApi";
-import { API } from "@/app/utils/api";
-import { mainApi } from "@/app/utils/mainApi";
-import { ReadBoardType } from "@/app/type/boardListType";
+import { modules } from "@/app/utils/reactQuillOptions";
+import ReactQuill from "react-quill";
 
-export default function PostUpdate() {
-  //임시 라우터 추후 API 연동 할 때 게시판명, id 분리해서 사용 하거나 상위에서 params 받게해서 처리
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+interface PostType {
+  post: {
+    title: string;
+    content: string;
+  };
+}
+
+export default function PostUpdate({ post }: PostType) {
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
   const [disabled, setDisabled] = useState(true);
+
   const boardId = useParams().boardId;
   const router = useRouter();
-
-  //게시글 상세보기
-  const getPostId = async (id: number) => {
-    const { POST_READ } = API.BOARD;
-    try {
-      const res = await mainApi({
-        url: POST_READ(String(id)),
-        method: "GET",
-      });
-      if (res.status === 200) {
-        return res.data as ReadBoardType;
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
   useEffect(() => {
     setDisabled(!title.trim() || !content.trim());
   }, [title, content]);
 
-  useEffect(() => {
-    const getPost = async () => {
-      const res = await getPostId(Number(boardId));
-      if (res) {
-        setTitle(res.title);
-        setContent(res.content);
-      }
-    };
-    getPost();
-  }, []);
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (disabled) {
       ErrorAlert("모든 필드를 다 채워주세요");
       return;
     }
+
     const formData = new FormData(e.target as HTMLFormElement);
     formData.append("content", content);
+
     const res = await updatePost(formData, Number(boardId));
+
     if (res === 200) {
       SuccessAlert("게시글 수정 성공");
       router.push("./");
       return;
     }
+
     if (res !== 200) {
       ErrorAlert("게시물 수정 실패");
     }
@@ -102,6 +84,7 @@ export default function PostUpdate() {
               modules={modules}
               className="h-full"
               value={content}
+              theme="snow"
             />
           </div>
         </div>
